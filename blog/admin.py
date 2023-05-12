cc1d0e5155158bf842976e5452acee9f564a3350
ali <laluallistilo@gmail.com>
2023-05-12 17:22:47 +0800

    add model manager to query

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,6 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['title','slug','author','publish','status']
     list_filter = ['status','created','publish','author']
     search_fields = ['title','body']
-    prepopulated_fields = {'slug':('title= ['author']
+    prepopulated_fields = {'slug':('title',)}
     date_hierarchy = 'publish'
     ordering = ['status','publish']
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+    
 
 class Post(models.Model):
 
@@ -11,12 +17,16 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    # 
 
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_created=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishManager()
 
     class Meta:
         ordering = ['-publish']
