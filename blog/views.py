15cbda8e200a9768e6abb28ff9541d6ec651f41b
ali <laluallistilo@gmail.com>
2023-05-15 17:17:25 +0800

    add model comment and generate to database

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Comment
 from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 def post_list(request):
     post_list = Post.published.all()
@@ -57,3 +58,14 @@ def post_share(request, post_id):
         form = EmailPostForm()
     return render(request,
                   'blog/post/share.html',{'post':post,'form':form,'sent':sent})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post=post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post':post},{'form':form}, {'comment':comment})
